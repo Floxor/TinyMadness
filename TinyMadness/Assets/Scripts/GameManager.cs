133406c6be153eba9c7a084c094293f52b64b7a7
Ttouaty <t.touaty@student.isartdigital.com>
2016-05-30 16:35:36 +0200

    Add basic UI, update git ignore

--- TinyMadness/Assets/Scripts/GameManager.cs
+++ TinyMadness/Assets/Scripts/GameManager.cs
@@ -3,14 +3,69 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+	private static GameManager _instance;
+	private bool _alreadyInstantiated;
+
+	void Awake()
+	{
+		if(_instance != null)
+			_alreadyInstantiated = true;
+		else
+			_instance = this;
+	}
 
 	void Start ()
 	{
-	
+		if (_alreadyInstantiated)
+			Destroy(gameObject);
 	}
 	
 	void Update ()
 	{
-	
+
 	}
+
+	public void GotoMainMenu()
+	{
+		Debug.Log("Going back to mainMenu");
+	}
+
+	public void LaunchGameOver()
+	{
+		Debug.Log("GAME LOST !");
+	}
+
+	public void StartCountDown()
+	{
+		StartCoroutine(CountDown());
+	}
+
+	IEnumerator CountDown()
+	{
+		yield return new WaitForSeconds(0.5f);
+		Debug.Log(3);
+		yield return new WaitForSeconds(0.5f);
+		Debug.Log(2);
+		yield return new WaitForSeconds(0.5f);
+		Debug.Log(1);
+		yield return new WaitForSeconds(0.5f);
+		Debug.Log("GO !");
+	}
+
+
+	public void StartTimeAttackGame()
+	{
+		Debug.Log("Start Time Attack Game");
+	}
+
+	public void StartSurvivalGame()
+	{
+		Debug.Log("Start Survival Attack Game");
+	}
+
+	public static GameManager GetInstance()
+	{
+		return _instance;
+	}
+
 }
---
+++ TinyMadness/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+	public GameObject activeMenuPanel;
+	public GameObject[] menuPanels;
+
+	void Start() 
+	{
+		StartCoroutine(delayedStart());
+	}
+
+	IEnumerator delayedStart()
+	{
+		yield return new WaitForSeconds(1);
+
+		if (activeMenuPanel != null)
+			bringInPanel(activeMenuPanel);
+		else if (menuPanels[0] != null)
+			bringInPanel(menuPanels[0]);
+		else
+			Debug.LogError("No default menu set and no panels to bring in for MenuManager !");
+	}
+
+	void Update() { }
+
+	public void bringInPanel(GameObject newMenuPanel)
+	{
+		MenuElement[] menuElements = newMenuPanel.GetComponentsInChildren<MenuElement>();
+
+		for (int i = 0; i < menuElements.Length; ++i)
+		{
+			menuElements[i].playInAnim();
+			if (menuElements[i].GetComponent<Button>() != null)
+				menuElements[i].GetComponent<Button>().interactable = true;
+		}
+
+		activeMenuPanel = newMenuPanel;
+	}
+
+	public void bringOutPanel(GameObject oldMenuPanel)
+	{ 
+		MenuElement[] menuElements = oldMenuPanel.GetComponentsInChildren<MenuElement>();
+
+		for (int i = 0; i < menuElements.Length; ++i)
+		{
+			menuElements[i].playOutAnim();
+			if (menuElements[i].GetComponent<Button>() != null)
+				menuElements[i].GetComponent<Button>().interactable = false;
+		}	
+	}
+
+	public void switchToPanel(GameObject newMenuPanel)
+	{
+		bringOutPanel(activeMenuPanel);
+		bringInPanel(newMenuPanel);
+	}
+}
